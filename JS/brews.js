e136666103cda711db9f6d75c8d6bd43d29f41f9
Arman Kazi <merl@Armans-MBP.san.rr.com>
2019-02-05 00:43:26 -0800

    worked on adding inputs to dialog - next task: dialog to db

--- JS/brews.js
+++ JS/brews.js
@@ -1,8 +1,29 @@
 
 const addBrewBtn = document.getElementById('brews.add-brew-button');
 const addBrewDialog = document.getElementById('brews.add-brew-dialog');
+const addBrewForm = document.getElementById('add-brew-dialog.form');
+const addBrewCancelBtn = document.getElementById('add-brew-dialog.cancel-button');
+const addBrewAddBtn = document.getElementById('add-brew-dialog.add-button');
+
+/* #region Button Protocols */
 
 addBrewBtn.addEventListener('click', e => {
     addBrewDialog.setAttribute('open', '');
     addBrewBtn.setAttribute('disabled', '');
 });
+
+addBrewCancelBtn.addEventListener('click', e => {
+    addBrewForm.reset();
+    document.getElementById('add-brew-dialog.brew-image-preview').src = "#";
+    addBrewDialog.removeAttribute('open');
+    addBrewBtn.removeAttribute('disabled');
+});
+
+/* #endregion Button Protocols */
+
+// Renders image in add brew dialog
+dialogUploadPreview = event => {
+    let preview = document.getElementById('add-brew-dialog.brew-image-preview');
+    preview.src = URL.createObjectURL(event.target.files[0]);
+    let image = event.target.files[0];
+};
