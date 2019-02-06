9289d8ef1aa86f9368f002de3bdd7e824f6ab577
Arman Kazi <merl@Armans-MBP.san.rr.com>
2019-02-05 22:18:49 -0800

    added user collection and document to db functionality --> isUser and addUser

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,12 @@
 /* #region Firestore Elements */
-
+const firestore = firebase.firestore();
+const settings = {/* your settings... */};
+firestore.settings(settings);
 const auth = firebase.auth();
+let user;
+let userData;
+let userEmail;
+let brewCount;
 
 /* #endregion Firestore Elements */
 
@@ -12,6 +18,12 @@ const exploreBtn = document.getElementById('nav.explore');
 // Authorization State Handler
 auth.onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
+
+        // store for db use
+        user = firebaseUser;
+        userEmail = firebaseUser.email;
+        isUser(userEmail);
+
         // Log Out Event Protocol
         logoutBtn.addEventListener('click', e =>{
             auth.signOut();
@@ -21,6 +33,34 @@ auth.onAuthStateChanged(firebaseUser => {
         window.location = 'login.html';
     }
 });
+
+// References for db
+let userCollect = firestore.collection('Users');
+let userDoc;
+
+function addUser() {
+    userDoc.set({
+        user: {
+            email: userEmail,
+            uid: user.uid
+        },
+        myBrewCount: 0
+    });
+}
+
+function isUser(email) {
+    userDoc = userCollect.doc('user-' + userEmail);
+    userDoc.get().then(function(doc)  {
+        // User exists
+        if (doc && doc.exists) {
+            userData = doc.data();
+            brewCount = userData.myBrewCount;
+        } 
+        else {
+            addUser();
+        }
+    });
+}
 
 /* #region Nav Functionality */
 
